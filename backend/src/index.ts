1b5e0ccf1df1b08ae01925e7458459eedc77d4ec
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2025-05-18 13:20:33 +0545

    BackendCode

---
+++ backend/src/index.ts
@@ -0,0 +1,67 @@
+import { WebSocket  ,  WebSocketServer} from 'ws'
+const wss = new  WebSocketServer({port:8080});
+
+type RequestMethod =  {
+ type: "join" | "chat",
+ payload:{
+    roomId: string |  ""
+    messege: string | ""
+ }  
+}
+
+type SocketArr  = {
+    roomId : string,
+    socket : WebSocket
+} 
+let socketArr : SocketArr[] = []; 
+
+wss.on("connection",(socket)=>{
+    socketArr.push({
+        roomId:"" ,
+        socket:socket 
+    });
+    socket.on("message",(messege)=>{
+        const stringMessege = messege.toString();
+
+        let actualJson: RequestMethod;
+       try {
+         actualJson  = JSON.parse(stringMessege);
+       }catch(error) {
+        console.log("error while getting the data ");
+        return
+       }
+        if(!actualJson){
+            console.log("Invalid Input");
+            return ; 
+        }
+       if( actualJson.type == "join"){
+        // Socket
+        const obtainedObjectindex : number  = socketArr.findIndex((element)=>element.socket==socket) 
+        socketArr[obtainedObjectindex].roomId = actualJson.payload.roomId ; 
+       } 
+
+       if(actualJson.type =="chat"){
+        
+            const filteredUser = socketArr.filter((elem)=>elem.socket == socket  
+            )
+            if(filteredUser[0].roomId ==""){
+                console.log("Cannot send messege until entering some room");
+                return ; 
+            }
+            socketArr.forEach((object)=>{
+                if(object.socket!=socket && object.roomId == filteredUser[0].roomId){
+                    object.socket.send(messege.toString());
+                }
+            })
+       }
+        // Handling of joining and sending
+       
+   }) 
+    socket.on("open",()=>{
+        socket.send("The chat is open");
+    }) ;  
+    socket.on("close",()=>{
+        socketArr =  socketArr.filter((element)=> { return element.socket != socket} )
+        console.log(` Socket array ${socketArr.toString()}`);
+    })
+})
